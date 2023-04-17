b5b4edcc98d57c1f0ee77037be565dbabb06c8fb
zuce68 <misiuk.pierre@gmail.com>
2023-04-17 16:00:12 +0200

    v1

--- traitesignfip.py
+++ traitesignfip.py
@@ -1,10 +1,3 @@
-import sounddevice as sd
-import soundfile as sf
-# Bibliothèques pour analyse traitement du signal
-import scipy.io.wavfile
-import matplotlib.pyplot as plt
-import numpy as np
-
 """
 TRAITESIGNFIP - Module Python pour les communications numériques à Télécom Physique Strasbourg (spécifiquement pour la formation FIP EII).
 
@@ -12,6 +5,17 @@ Copyright Université de Strasbourg 2023 (2023-03-15)
 Contributeur : pierre.misiuk@etu.unistra.fr
                mathieu.schwoerer@etu.unistra.fr
 """
+
+import sounddevice as sd
+import soundfile as sf
+# Bibliothèques pour analyse traitement du signal
+import scipy.io.wavfile
+import matplotlib.pyplot as plt
+import numpy as np
+
+import threading
+
+
 
 def play_signal(signal, sample_rate):
     """
@@ -28,12 +32,13 @@ def play_signal(signal, sample_rate):
     sd.wait()
 
 # Fonction pour enregistrer le son du microphone
-def record_microphone(signal_type):
+def record_microphone(signal_type,time):
     """
     Acquisition du son via microphone.
     
     Entrées :
-    signal_type (string)      : nom du signal envoyé sur l'haut-parleur pour l'acquérir avec les micros.
+    signal_type (string)      : nom du signal envoyé sr l'haut-parleur pour l'acquérir avec les micros.
+    time (decimal) : valeur de l'enregistrement en seconde 
 
     signal_type:
     -noise : bruit blanc.
@@ -44,25 +49,29 @@ def record_microphone(signal_type):
     aucune
     """
     global samples
-    duration = 5  # Durée de l'enregistrement en secondes
+    if time==0:
+        duration=5
+    else:
+        duration = int((5*time)/3)  # Durée de l'enregistrement en secondes
     sample_rate = 44100  # Fréquence d'échantillonnage en Hz
     channels = 2
-    t = np.linspace(0,1,int(sample_rate),endpoint=False)
+
+    t = np.linspace(0,1*time,int(sample_rate),endpoint=False)
  
     
     if signal_type == "noise":
-        zeros = np.zeros(3*sample_rate)
-        samples = np.random.normal(0, 1, int(44100))
-        signal = np.concatenate((zeros,samples,np.zeros(sample_rate)))
+        zeros = np.zeros((duration-time)*sample_rate)
+        samples = np.random.normal(0, 1, time*sample_rate)
+        signal = np.concatenate((zeros,samples))
     elif signal_type == "sinus":
-        zeros = np.zeros(3*sample_rate)
+        zeros = np.zeros((duration-time)*sample_rate)
         frequency = 440  # Hz
-        samples = (np.sin(2 * np.pi * np.arange(sample_rate ) * frequency / sample_rate)).astype(np.float32)
-        signal = np.concatenate((zeros,samples,np.zeros(sample_rate)))
+        samples = (np.sin(2 * np.pi * np.arange(sample_rate * time ) * frequency / sample_rate)).astype(np.float32)
+        signal = np.concatenate((zeros,samples))
     elif signal_type == "clap":
-        zeros = np.zeros(3*sample_rate)
-        handclap=np.sin(2*np.pi*500*t)*np.exp(-5*t)
-        signal = np.concatenate((zeros,handclap,np.zeros(sample_rate)))
+        zeros = np.zeros((duration-time)*sample_rate)
+        samples=np.sin(2*np.pi*500*t)*np.exp(-5*t)
+        signal = np.concatenate((zeros,samples))
     else:
         print("Type de signal non reconnu.")
         return
